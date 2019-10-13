944256cc93bcd98c94901a33abe2f4a5369e60ef
nitin023 <khulbe.nitin@gmail.com>
2019-10-13 18:53:44 +0530

    Group Anagram problem solved

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        String[] strs = {"cab","tin","pew","duh","may","ill","buy","bar","max","doc"};
 
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(1);
-        System.out.println(TraversalUtil.isValidBST(root));
+        Map<String, List<String>> grpMap = new HashMap<>();
+        for(String s : strs)
+        {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String sortedStr = String.valueOf(arr);
+            List<String>tList = new ArrayList<>();
+            if(grpMap.containsKey(sortedStr))
+            {
+               tList = grpMap.get(sortedStr);
+            }
+            tList.add(s);
+            grpMap.put(sortedStr,tList);
+        }
+        List<List<String>> responseList = new ArrayList<>();
+        for(Map.Entry<String, List<String>> m : grpMap.entrySet())
+        {
+            responseList.add(m.getValue());
+        }
     }
 }
