819f07a829b2ec27f6dee27bae52623cd32bb5e3
nitin023 <khulbe.nitin@gmail.com>
2019-11-10 10:24:49 +0530

    searching in a 2D matrix using optimised binary search

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList resp  = Searching.searchRange(new ArrayList<>(Arrays.asList(1,1,2,2,3)),1);
-        System.out.println(resp.get(0) + "-----------" + resp.get(1));
+        ArrayList<ArrayList<Integer>> a = new ArrayList<>();
+        a.add(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+        a.add(new ArrayList<>(Arrays.asList(7,8,9,10,11,12)));
+        a.add(new ArrayList<>(Arrays.asList(13,14,15,16,17,18)));
+        a.add(new ArrayList<>(Arrays.asList(20,21,22,23,24,25)));
+        a.add(new ArrayList<>(Arrays.asList(26,27,28,29,30,31)));
+        a.add(new ArrayList<>(Arrays.asList(32,33,34,35,36,37)));
+        a.add(new ArrayList<>(Arrays.asList(38,39,40,41,42,43)));
+
+        System.out.println(Searching.searchMatrix(a,56));
     }
 }
--- src/Searching.java
+++ src/Searching.java
@@ -44,4 +44,65 @@ public class Searching {
         resp.add(last(a,0,a.size()-1,b));
         return resp;
     }
+
+    public static int searchMatrix(ArrayList<ArrayList<Integer>> a, int b)
+    {
+        int low = 0 , high = a.size() -1;
+        while (low <= high)
+        {
+            int mid = (low+high)/2;
+            ArrayList<Integer>rangeList = a.get(mid);
+            int rLeft =  rangeList.get(0);
+            int rRight =  rangeList.get(rangeList.size() - 1);
+            if(rLeft <=b && b<=rRight)
+            {
+                int index = binarySearch(rangeList,0,rangeList.size()-1,b);
+                if(index>=0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                //mid largest value is < b --- mid+1
+                if(b>rRight)
+                {
+                    low = mid +1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static int binarySearch(ArrayList<Integer>array , int low , int high , int key)
+    {
+        if(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(array.get(mid) == key)
+            {
+                return mid;
+            }
+            else if(key <array.get(mid))
+            {
+                return binarySearch(array , low,mid -1 , key);
+            }
+            else
+            {
+                return binarySearch(array ,mid +1 ,high , key);
+            }
+        }
+        else
+        {
+            return -1;
+        }
+    }
 }
